ad6579e89934c4fcc22c5e8543b6a4c21c623dc3
juangrajalesmonto2-afk <juangrajalesmonto2@gmail.com>
2026-09-11 14:35:31 -0500

    SRC-001 v1.0 - codigo fuente inicial
    Implementación de la clase Estudiante coherente con REQ-001 y DIS-001

---
+++ 02_Codigo_Fuente/SRC-001_Gestion_Estudiantes.py
@@ -0,0 +1,6 @@
+# Ejemplo mínimo de SRC-001 v1.0
+class Estudiante:
+    def __init__(self, identificacion, nombre_completo, correo_electronico):
+        self.identificacion = identificacion
+        self.nombre_completo = nombre_completo
+        self.correo_electronico = correo_electronico
